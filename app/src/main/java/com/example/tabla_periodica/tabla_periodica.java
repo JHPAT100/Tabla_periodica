f4b802b66fae71a625a0070b04c15828976f2d99
JHPAT <sanhool@live.com.mx>
2019-05-14 20:24:56 -0500

    implementando el metodo onclick que ejecuta multiples botones

--- app/src/main/java/com/example/tabla_periodica/tabla_periodica.java
+++ app/src/main/java/com/example/tabla_periodica/tabla_periodica.java
@@ -1,30 +1,30 @@
 package com.example.tabla_periodica;
 
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
 
 public class tabla_periodica extends AppCompatActivity {
     public Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabla_periodica);
 
-        for (int i=1; i<15; i++){
-            int buttonId = this.getResources().getIdentifier("b_"+i, "id", this.getPackageName());
-            Button btn = (Button)findViewById(buttonId);
-            
-        }
+
+    }
+
+    public void onClick(View view){
+        btn=findViewById(view.getId());
+        Intent onClick=new Intent(this,prueba.class);
+        startActivity(onClick);
 
     }
 
 }
+
 
