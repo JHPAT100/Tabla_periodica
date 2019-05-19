65e3bfbdf6ed1d018af542dab828b4e29e0b5608
JuanTorresYah <juan.torres@cbtis72.edu.mx>
2019-05-19 12:35:30 -0500

    Infomación del 46 al 91

--- app/src/main/java/com/example/tabla_periodica/busqueda.java
+++ app/src/main/java/com/example/tabla_periodica/busqueda.java
@@ -19,16 +19,105 @@ public class busqueda extends AppCompatActivity {
 
     String elementos[][] = {
 //Nombre,descripción,simbolo,grupo,bloque,masa,protones,electrones,neutrones,valencia,
-            {"Paladio", "Hola este elemento esta vacio", "46", "Pd", "VIIIB", "d", "106.42(g/mol)", "46", "46", "60", "+2, +4, +6"},
-            {"Plata", "Hola este elemento esta vacio","47", "Ag", "IB", "d", "107.868(g/mol)", "47", "47", "61", "+1, +2"},
+
+             //46-91
+                      {"Paladio","Es un raro y brillante metal de color plateado, fue nombrado así por el asteroide Pallas.","46","Pd","VIIIB","d","106.42(g/mol)","46","46","60","+2, +4, +6"},
+
+                      {"Plata","Es un metal de transición suave, de color blanco y brillante, posee la mas alta conductividad eléctrica, térmica y reflectiva entre los metales.","47","Ag","IB","d","107.868(g/mol)","47","47","61","+1, +2"},
+
+                      {"Cadmio","Es un metal suave, de color blanco-azulado, químicamente es similar al Zinc y al Mercurio, tiene un  bajo punto de fusión a comparación con otros metales.","48","Cd","IIB","d","112.411(g/mol)","48","48","64","+1, +2"},
+
+                      {"Indio","Es un metal de transición suave, maleable y con un bajo punto de fusión, el cual es más alto que el del Sodio pero más bajo que el del Litio o el Estaño.","49", "In","P","IIIA","114.818(g/mol)","49","49","66","+1, +2, +3"},
+
+                      {"Estaño","Muestra un similitud química con el Germanio y el Plomo, forma parte de grupo principal en el grupo 14 de la tabla periódica.","50","Sn","p","IVA","118.710(g/mol)","50","50","69","+2, +4"},
+
+                      {"Antimonio","Es un metaloide gris brillante, se encuentra principalmente en la naturaleza como el mineral sulfuro de estibina, sus compuestos se conocen desde la antiguedad y eran usados en cosméticos.","51","Sb","P","VA","121.760(g/mol)","51","51","71","-3, +3, +4, +5"},
+
+                      {"Telurio","Es un metaloide de color blanco-plateado, frágil, levemente tóxico y raro, químicamente esta relacionado con el Selenio y el Azufre.","52","Te","p","VIA","127.60(g/mol)","52","52","75","-2, +2, +4, +6"},
+
+                      {"Yodo","El yodo y sus compuestos se utilizan en nutrición, en la producción de ácido acético y ciertos polimeros, su nombre proviene del griego y significa violeta o purpura, debido al color del vapor del Yodo","53","I","p","VIIA","126.904(g/mol)","53","53","74","-1, +1, +3, +4, +5, +7"},
+
+                      {"Xenón","Es un gas noble incoloro, denso e inodoro, se produce en la atmósfera de la tierra  en cantidades mínimas","54","Xe","p","VIIA","131.293(g/mol)", "54","54","77","0"},
+
+                      {"Cesio","Es un metal alcalino suave de color dorado-plateado, su punto de fusión es de 28c°, es uno de los 5 metales que se encuentran en estado líquido en o cerca de la temperatura ambiente.","55","Cs","s","IA","132.905(g/mol)","55","55","78","+1"},
+
+                      {"Bario","Es un metal alcalinotérreo de color plateado métalico, debido a su alta reactividad química nunca se encuentra como un elemento libre en la naturaleza.","56","Ba","s","IIA","137.327(g/mol)","56","56","81","+2"},
+
+                      {"Lantano","Es un metal  blando, dúctil, de color blanco-plateado, se lustra rapidamente cuando se expone al aire y es lo suficientemente suave para cortarlo con un cuchillo.","57","La","d","IIB","138.905(g/mol)", "57","57","82","+3"},
+
+                      {"Cerio","Es un metal blando, plateado y dúctil el cual se oxida fácilmente al exponerlo al aire.","58","Ce","f","-","140.116(g/mol)", "58","58","82","+3, +4"},
+
+                      {"Praseodimio","Es un metal blando, plateado, dúctil y maleable, es valorado por sus propiedades magnéticas, eléctricas, químicas y ópticas.","59","Pr","f","-","140.904(g/mol)","59","59","82","+3"},
+
+                      {"Neodimio","Es un metal de color plateado, suave que se deslustra con el aire.","60","Nd","f","-","144.242(g/mol)","60","60","84","+3, +4"},
+
+                      {"Prometio","Químicamente es un lantanido que forma sales cuando se combina con otros elementos, todos sus isótopos son radioactivos.","61","Pm","f","-","146.915(g/mol)","61","61","85","+3"},
+
+                      {"Samario","Es un metal plateado moderadamente duro que se oxida fácilmente con el aire.","62","Sm","f","-","150.36(g/mol)","62","62","88","+2, +3"},
+
+                      {"Europio","Es un metal moderadamente duro que se oxida fácilmente con el aire y el agua, lleva el nombre de el continente de Europa.","63","Eu","f","-","151.964(g/mol)","63","63","89","+2, +3"},
+
+                      {"Gadolinio","Es un metal de tierra rara, de color blanco-plateado, maleable y dúctil, en la naturaleza solo se encuentra de forma combinada.","64","Gd","f","-","157.25(g/mol)","64","64","93","+3"},
+
+                      {"Terbio","Es un metal de tierra rara de color blanco-plateado,maleable, dúctil y lo suficientemente suave para cortarlo con un cuchillo, nunca se encuentra en la naturaleza como un elemento libre.","65","Tb","f","-","158.925(g/mol)","65","65","93","+3, +4"},
+
+                      {"Disprosio","Es un elemento de tierra rara con un brillo metálico plateado, nunca se encuentra en la naturaleza como un elemento libre.","66","Dy","f","-","162.500(g/mol)","66","66","96","+3"},
+
+                      {"Holmio","Elemento de tierra rara, forma parte de el grupo de los lantanidos","67","Ho","f","-","164.93(g/mol)","67","67","97","+3"},
+
+                      {"Erbio","Metal solido de color blanco-plateado cuando se encuentra aislado artificialmente, es un elemento de tierra rara que se asocia con otros minerales de raros en el mineral gadolinita de Ytterby","68","Er","f","-","167.259(g/mol)","68","68","99","+3"},
+
+                      {"Tulio","Es el antepenultimo de la serie de los lantanidos y aligual que los otros lantanidos su estado de oxidación mas común es +3","69","Tm","f","-","168.934(g/mol)","69","69","99","+2, +3"},
+
+                      {"Iterbio","Es el penúltimo de la serie de los lantánidos, al igual que los otros lantánidos su estado de oxidación mas común es +3, visto en su óxido, haluros y otros compuestos.","70","Yb","f","-","173.04(g/mol)","70","70","103","+2, +3"},
+
+                      {"Lutecio","Es uno de los elementos del bloque d pero con freciencia se le suele incluir en los lantánidos debido a que comparte muchas propiedades con estos, de todos ellos es el mas difícil de aislar.","71","Lu","f","-","174.967(g/mol)","71","71","103","+3"},
+
+                      {"Hafnio","Es un metal de transición tetravalente, brillante, plateado y de color gris, se asemeja químicamente al Circonio y se encuentra en minerales de circonio.","72","Hf","d","IVB","178.49(g/mol)","72","72","106","+4"},
+
+                      {"Tantalo","Su nombre proviene de Tántalo, un antiheroe de la mitología griega, es un metal de transición lustroso, raro, duro, de color gris_azulado que es altamente resistente a la corrosión. ","73","Ta","d","VB","180.947(g/mol)","73","73","108","+3, +4, +5"},
+
+                      {"Wolframio","Tambien conocido como tungsteno, palabra que proviene de la lengua sueca tung sten que significa piedra pesada.","74","W","d","VIB","183.84(g/mol)","74","74","109","+2, +3, +4, +5, +6"},
+
+                      {"Renio","Es un metal de transición de la tercera fila, grueso y de color plateado, es uno de los elementos más raros en la corteza terrestre.","75","Re","d","VIIB","186.207(g/mol)","75","75","111","-1, +2, +3, +4, +5, +6, +7"},
+
+                      {"Osmio","Es un metal de transición de color blanco_azulado, duro y frágil en el grupo del platino que se encuentra como un elemento traza en las aleaciones. Es el elemento natural más denso.","76","Os","d","VIIIB","190.23(g/mol)","76","76","115","+2, +3, +4, +6, +8"},
+
+                      {"Iridio","Es unmetal de transición duro, frágil, de color platino-blanco, generalmente se le atribuye como el segundo elemento  más denso  basado en la densidad medida.","77","Ir","d","VIIIB","192.217(g/mol)","77","77","115","+1, +2, +3, +4, +6"},
+
+                      {"Platino","Es unmmetal de transición denso, maleable, dúctil, altamente no reactivo, precioso y de color gris-blanco, su nombre deriva del termino español platina, que se traduce como poca plata.  ","78","Pt","d","VIIIB","195.084(g/mol)","78","78","117","+1, +2, +3, +4, +6"},
+
+                      {"Oro","En su forma más pura, es un metal brillante, ligeramente rojizo, denso, suave, maleable y dúctil, químicamente es un metal de transición.","79","Au","d","IB","196.966(g/mol)","79","79","118","+1, +2, +3"},
+
+                      {"Mercurio","Es un elemento del bloque d pesado y de color plateado, es el único elemento metálico que es líquido en condiciones estándar de temperatura y presión.","80","Hg","d","IIB","200.59(g/mol)","80","80","120","+1, +2"},
+
+                      {"Talio","Es un metal de transición suave y de color gris, no se encuentra libre en la naturaleza, cuando se encuentra aislado, se parece al estaño pero se decolora al exponerlo al aire.","81","Tl","p","IIIB","204.383(g/mol)","81","81","123","+1, +2, +3"},
+
+                      {"Plomo","Es u metal blando, maleable y pesado despues de la transición, al exponerlo al aire se torna de un color gris o opaco.","82","Pb","p","IVA","207.2(g/mol)", "82","82","125","+2, +4"},
+
+                      {"Bismuto","Es unmetal de transición posterior pentavalente, químicamente se asemeja al arsénico y el antimonio, los cristales de bismuto  suelen adoptar formas geométricas y poseen una amplia grama de colores.","83","Bi","p","VA","208.9804(g/mol)","83","83","126","-3, +2, +3, +4, +5"},
+
+                      {"Polonio","Es un elemento raro y altamente radioactivo sin isótopos estables, es químicamente similar al bismuto y al telurio, sus aplicaciones son pocas.","84","Po","p","VIB","208.9824(g/mol)","84","84","126","-2, +2, +4, +6"},
+
+                      {"Ástato","Ocurre en la Tierra como el producto de desintegración de varios elementos más pesados. Todos sus isótopos son de corta duración.","85","At","p","VIIA","209.9871(g/mol)","85","85","125","-"},
+
+                      {"Radón","Es un gas noble radioactivo, incoloro, inodoro e insípido, que se produce naturalmente como producto de la desintegración del radio.","86","Rn","p","VIIIB","222.0176(g/mol)","86","86","136","0"},
+
+                      {"Francio","Es el segundo elemento menos electronegativo, solo detras del cesio, es un metal altamente radioactivo que se descompone en astatino, radio y radón.","87","Fr","s","IA","223.0197(g/mol)","87","87","136","-"},
+
+                      {"Radio","El radio puro es casi incoloro, pero se combina fácilmente con nitrógeno en la exposición al aire, formando una capa negra de nitruro de radio.","88","Ra","s","IIA","226.0254(g/mol)","88","88","138","+2"},
+
+                      {"Actinio","Fue el primer elemento radioactivo no primordial en aislarse, due descubierto en 1899.","89","Ac","d","IIIB","227.0278(g/mol)","89","89","138","+3"},
+
+                      {"Torio","Es uno de los 2 elementos significativamente radioactivos que aún ocurren naturalmente en grandes cantidades como elemento primordial, el otro es el Uranio.","90","Th","f","-","232.0380(g/mol)","90","90","142","+4"},
+
+                      {"Protactinio","Es un metal denso de color gris-plateado que reacciona fácilmente con el oxígeno, el vapor de agua y los ácidos inorgánicos.","91","Pa","f","-","231.0358(g/mol)","91","91","140","+5"},
 
 
+                    //92-118
 
 
-            //92-118
-
-
-            {"Uranio","El uranio tiene el mayor peso atómico de entre todos los elementos que se encuentran en la naturaleza.","U","7","f","238.03g/mol","92","143","3,4,5,6"},
+                    {"Uranio","El uranio tiene el mayor peso atómico de entre todos los elementos que se encuentran en la naturaleza.","U","7","f","238.03g/mol","92","143","3,4,5,6"},
 
                     {"Neptunio","Su nombre proviene del planeta Neptuno.","Np","7","f","237g/mol","93","--","144","3,4,5,6"},
 
@@ -82,7 +171,32 @@ public class busqueda extends AppCompatActivity {
 
                     {"Oberón","El oberón o ununoctio (conocido anteriormente como eka-radón); nombre sistemático temporal dado por la Unión Internacional de Química Pura y Aplicada al elemento sintético transactínido","On","7","f","288g/mol","118","--","169","4,7"}
     };
-    int[] datosImg = {R.drawable.icono_b, R.drawable.logo,};
+
+    //falta los elementos 94, 95, 106,112,113, 114, 115, 116,117, 118, 119 al 123
+    int[] datosImg = {R.drawable.icono_b, R.drawable.logo,R.drawable.e_46,R.drawable.e_47,R.drawable.e_48,
+
+            R.drawable.e_49,R.drawable.e_50,R.drawable.e_51,R.drawable.e_52,R.drawable.e_53,R.drawable.e_54,
+
+            R.drawable.e_55,R.drawable.e_56,R.drawable.e_57,R.drawable.e_58,R.drawable.e_59,R.drawable.e_60,
+
+            R.drawable.e_61,R.drawable.e_62,R.drawable.e_63,R.drawable.e_64,R.drawable.e_65,R.drawable.e_66,
+
+            R.drawable.e_67,R.drawable.e_68,R.drawable.e_69,R.drawable.e_70,R.drawable.e_71,R.drawable.e_72,
+
+            R.drawable.e_73,R.drawable.e_74,R.drawable.e_75,R.drawable.e_76,R.drawable.e_77,R.drawable.e_78,
+
+            R.drawable.e_79,R.drawable.e_80,R.drawable.e_81,R.drawable.e_82,R.drawable.e_83,R.drawable.e_84,
+
+            R.drawable.e_85,R.drawable.e_86,R.drawable.e_87,R.drawable.e_88,R.drawable.e_89,R.drawable.e_90,
+
+            R.drawable.e_91,R.drawable.uranio,R.drawable.neptunio,R.drawable.curio,R.drawable.berkelio,
+
+            R.drawable.californio,R.drawable.einstenio,R.drawable.fermio,R.drawable.mendelevio,R.drawable.nobelio,
+
+            R.drawable.laurencio,R.drawable.ruthefordio,R.drawable.dubnio,R.drawable.bohrio,R.drawable.hassio,
+
+            R.drawable.meitnerio,R.drawable.darmstadtio,R.drawable.roentgenio
+    };
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
