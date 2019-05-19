b36e7e9a3e3b40bc710ac53b805d45434a6f9106
MARIA <majoibarra1022@gmail.com>
2019-05-19 18:24:32 -0500

    actualizado

--- app/src/main/java/com/example/tabla_periodica/busqueda.java
+++ app/src/main/java/com/example/tabla_periodica/busqueda.java
@@ -137,39 +137,39 @@ public class busqueda extends AppCompatActivity {
 
                     {"Mendelevio","El nombre de este elemento proviene del creador de la Tabla periódica de los Elementos: Dmitri Mendeleyev","Md","7","f","258g/mol","101","--","152","--"},
 
-                    {"Nobelio","Hasta la fecha sólo se han producido cantidades atómicas del elemento. El nobelio es el décimo elemento más pesado","No","7","f","259g/mol","102","--","153","--"},
+                    {"Nobelio","Hasta la fecha sólo se han producido cantidades atómicas del elemento. El nobelio es el décimo elemento más pesado","No","7","d","259g/mol","102","--","153","--"},
 
-                    {"Laurencio","El laurencio, nombrado así en honor de E. O. Lawrence, es el undécimo elemento transuránico","Lr","7","f","262g/mol","103","--","154","--"},
+                    {"Lawrencio","El lawrencio, nombrado así en honor de E. O. Lawrence, es el undécimo elemento transuránico","Lr","7","d","262g/mol","103","--","154","--"},
 
-                    {"Ruthefordio","u nombre fue elegido en honor del Barón Ernest Rutherford, científico colaborador del modelo atómico y física nuclear.","Rf","7","f","261g/mol","104","--","155","--"},
+                    {"Rutherfordio","Su nombre fue elegido en honor del Barón Ernest Rutherford, científico colaborador del modelo atómico y física nuclear.","Rf","7","d","261g/mol","104","--","155","--"},
 
-                    {"Dubnio","El elemento fue nombrado en honor a la ciudad de Dubna, Rusia, donde fue producido por primera vez","Db","7","f","262g/mol","105","--","156","--"},
+                    {"Dubnio","El elemento fue nombrado en honor a la ciudad de Dubna, Rusia, donde fue producido por primera vez","Db","7","d","262g/mol","105","--","156","--"},
 
-                    {"Seaborgio","Es un elemento sintético cuyo isótopo más estable y que tiene una vida media de 2,4 minutos. ","Sg","7","f","263g/mol","106","--","157","--"},
+                    {"Seaborgio","Es un elemento sintético cuyo isótopo más estable y que tiene una vida media de 2,4 minutos. ","Sg","7","d","263g/mol","106","--","157","--"},
 
-                    {"Bohrio","Su nombre le fue dado en honor al científico danés Niels Bohr. Elemento químico que se espera que tenga propiedades químicas semejantes a las del elemento renio.","Bh","7","f","262g/mol","107","--","158","--"},
+                    {"Bohrio","Su nombre le fue dado en honor al científico danés Niels Bohr. Elemento químico que se espera que tenga propiedades químicas semejantes a las del elemento renio.","Bh","7","d","262g/mol","107","--","158","--"},
 
-                    {"Hassio","El hasio, ​ anteriormente conocido provisionalmente como unniloctio, ​​, es un elemento sintético de la tabla periódica","Hs","7","f","262g/mol","108","--","159","--"},
+                    {"Hasio","El hasio, ​ anteriormente conocido provisionalmente como unniloctio, ​​, es un elemento sintético de la tabla periódica","Hs","7","d","262g/mol","108","--","159","--"},
 
-                    {"Meitnerio","El meitnerio (anteriormente llamado unnilenio con símbolo provisional Une o Wolschakio con símbolo Wl al no ser oficial)​​ es un elemento químico","Mt","7","f","266g/mol","109","--","160","--"},
+                    {"Meitnerio","El meitnerio (anteriormente llamado unnilenio con símbolo provisional Une o Wolschakio con símbolo Wl al no ser oficial)​​ es un elemento químico","Mt","7","d","266g/mol","109","--","160","--"},
 
-                    {"Darmstadtio","El darmstatio​ (anteriormente llamado ununnilio con símbolo provisional Uun o madveded, madvedevio o medvedio​ con símbolo Mv al no ser oficial)​​ es un elemento químico","Ds","7","f","281g/mol","110","--","161","2,4"},
+                    {"Darmstadtio","El darmstatio​ (anteriormente llamado ununnilio con símbolo provisional Uun o madveded, madvedevio o medvedio​ con símbolo Mv al no ser oficial)​​ es un elemento químico","Ds","7","d","281g/mol","110","--","161","2,4"},
 
-                    {"Roentgenio","También tenía asignado el nombre de plutirio con el símbolo Pl al no ser oficial​. Fue descubierto en 1994 por científicos alemanes en Darmstadt.","Rg","7","f","272g/mol","111","--","162","3,2"},
+                    {"Roentgenio","También tenía asignado el nombre de plutirio con el símbolo Pl al no ser oficial​. Fue descubierto en 1994 por científicos alemanes en Darmstadt.","Rg","7","d","272g/mol","111","--","162","3,2"},
 
-                    {"Darwanizo","El ununbio (Uub) es un nombre temporal para referirse al elemento químico","Da","7","f","269g/mol","112","--","163","4,5,4,3"},
+                    {"Copernico","El ununbio (Uub) es un nombre temporal para referirse al elemento químico","Cn","7","p","269g/mol","112","--","163","4,5,4,3"},
 
-                    {"Tusfrano","El tusfrano es un elemento químico radiactivo","Tf","13","f","272g/mol","113","--","164","3"},
+                    {"Nohonio","El tusfrano es un elemento químico radiactivo","Nh","13","p","272g/mol","113","--","164","3"},
 
-                    {"Erristeneo"," Elemento químico relativamente nuevo, cuyo descubrimiento se atribuye a los rusos. ","Eo","7","f","276g/mol","114","--","165","4,6,4,2"},
+                    {"Erristeneo"," Elemento químico relativamente nuevo, cuyo descubrimiento se atribuye a los rusos. ","Eo","7","p","276g/mol","114","--","165","4,6,4,2"},
 
-                    {"Merchel","es un combustible magnifico ya que no es de este planeta, ya que fue encontrado en naves extraterrestre","Me","7","f","279g/mol","115","--","166","5,4,5,1"},
+                    {"Merchel","es un combustible magnifico ya que no es de este planeta, ya que fue encontrado en naves extraterrestre","Me","7","p","279g/mol","115","--","166","5,4,5,1"},
 
-                    {"Nectarten","El Nectarien es un elemento químico que no está aprobado oficialmente por la IUPAC y que no tiene una fecha determinada de su descubrimiento.","Nc","7","f","282g/mol","116","--","167","5,4,5,3"},
+                    {"Nectarten","El Nectarien es un elemento químico que no está aprobado oficialmente por la IUPAC y que no tiene una fecha determinada de su descubrimiento.","Nc","7","p","282g/mol","116","--","167","5,4,5,3"},
 
-                    {"Efelio","Este nuevo elemento químico fue creado al hacer colisionar isótopos de calcio","El","7","f","285g/mol","117","--","168","2,3,2,1"},
+                    {"Efelio","Este nuevo elemento químico fue creado al hacer colisionar isótopos de calcio","El","7","p","285g/mol","117","--","168","2,3,2,1"},
 
-                    {"Oberón","El oberón o ununoctio (conocido anteriormente como eka-radón); nombre sistemático temporal dado por la Unión Internacional de Química Pura y Aplicada al elemento sintético transactínido","On","7","f","288g/mol","118","--","169","4,7"}
+                    {"Oberón","El oberón o ununoctio (conocido anteriormente como eka-radón); nombre sistemático temporal dado por la Unión Internacional de Química Pura y Aplicada al elemento sintético transactínido","On","7","p","288g/mol","118","--","169","4,7"}
     };
 
     //Se agregaron todas las imagenes
