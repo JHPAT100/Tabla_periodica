55af6679d2d27cc7eb4424731d9f8741c9ed2297
JHPAT <sanhool@live.com.mx>
2019-05-19 21:13:59 -0500

    Cracion del metodo que inserta los datos en la vista modificaiones al diseño

--- app/src/main/java/com/example/tabla_periodica/tabla_periodica.java
+++ app/src/main/java/com/example/tabla_periodica/tabla_periodica.java
@@ -3,8 +3,6 @@ package com.example.tabla_periodica;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -12,17 +10,15 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
+
 
 public class tabla_periodica extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener ,tabla.OnFragmentInteractionListener{
-Button btn;
-
+    public static CharSequence indetificador;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -101,6 +97,7 @@ Button btn;
     }
     public void onClick(View view){
         btn=findViewById(view.getId());
+      indetificador= btn.getText();
         Intent onClick=new Intent(this,prueba.class);
         startActivity(onClick);
 
